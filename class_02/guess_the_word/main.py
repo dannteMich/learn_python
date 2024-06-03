87c361a48bee4867badbb0c12e6899a2cb82ff7b
Michael R <michael.r@spherex.xyz>
2024-06-03 23:37:24 +0300

    part 1 finished

---
+++ class_02/guess_the_word/main.py
@@ -0,0 +1,48 @@
+
+def print_partial_word(word: str, guessed_letters: list[str]):
+    res = ''
+    for letter in word:
+        if letter in guessed_letters:
+            res = res + letter
+        else:
+            res = res + "*"
+    
+    print(res)
+
+def get_single_letter_from_user(guessed_letters: list[str]) -> str:
+    while True:
+        letter = input("Please provide a single letter: ").upper()
+        if len(letter) != 1:
+            print("The input should be 1 letter. No more and no less")
+        elif not letter.isalpha():
+            print("The input should be only alphabetical letters")
+        elif letter in guessed_letters:
+            print("This letter was already guessed")
+        else:
+            return letter
+
+def is_word_fully_guessed(word: str, guessed_letters: list[str]):
+    for letter in word:
+        if not letter in guessed_letters:
+            return False
+    
+    return True
+
+def guess_the_word(word_to_guess: str):
+    # TODO: handle guessing the same letter twice
+    word_to_guess = word_to_guess.upper()
+    guessed_letters = []
+
+    while not is_word_fully_guessed(word_to_guess, guessed_letters):
+        print_partial_word(word_to_guess, guessed_letters)
+        letter = get_single_letter_from_user(guessed_letters)
+        guessed_letters.append(letter)
+
+    print("Congratulations!!!")
+        
+
+
+if __name__ == "__main__":
+    print("Starting program\n")
+
+    guess_the_word("system")
