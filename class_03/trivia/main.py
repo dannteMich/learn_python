4d7992ca649a6092ee2c52b1acf2b3325629f376
Michael R <michael.r@spherex.xyz>
2024-06-21 14:30:21 +0300

    finished part 2

--- class_03/trivia/main.py
+++ class_03/trivia/main.py
@@ -1,44 +1,17 @@
-def print_question(question: str, answers: list[str]):
-    print(f"Question: {question}")
-    for i, answer in enumerate(answers, 1):
-        print(f"\t{i}. {answer}")
-
-
-def get_answer_index_from_user(answers_num: int): 
-    while True:
-        raw_input = input("Please select an answer (number): ")
-        if not raw_input.isdigit():
-            print("Bad input. Please provide a positive number")
-            continue
-        input_number = int(raw_input)
-        if input_number < 1 or input_number > answers_num:
-            print(f"Bad input. Please provide numbers in range [1, {answers_num}]")
-            continue
-
-        return input_number - 1
-
-
-
-def ask_a_question(question: str, answers: list[str], correct_index: int):
-
-    print_question(question, answers)
-    answer_index = get_answer_index_from_user(len(answers))
-
-    if answer_index == correct_index:
-        return True
-    return False
+from question import Question
 
 
 if __name__ == "__main__":
     print("Starting Game")
 
-    success = ask_a_question(
+    q1 = Question(
         "What is the capital of Brazil",
         ["São Paulo", "Rio de Janeiro", "Brasília", "Salvador"],
         2,
     )
 
+    success = q1.ask_a_question()
     if success:
         print("You are correct")
     else:
-        print("You are a failure")
+        print("You failed.")
---
+++ class_03/trivia/question.py
@@ -0,0 +1,36 @@
+class Question:
+
+    def __init__(self, question: str, answers: list[str], correct_index: int):
+        self._question = question
+        self._answers = answers
+        self._correct_index = correct_index
+
+    def print_question(self):
+        print(f"Question: {self._question}")
+        for i, answer in enumerate(self._answers, 1):
+            print(f"\t{i}. {answer}")
+
+    def ask_a_question(self):
+
+        self.print_question()
+        answer_index = self._get_answer_index_from_user()
+
+        if answer_index == self._correct_index:
+            return True
+        return False
+
+    def _get_answer_index_from_user(self):
+        answers_num = len(self._answers)
+
+        while True:
+            raw_input = input("Please select an answer (number): ")
+            if not raw_input.isdigit():
+                print("Bad input. Please provide a positive number")
+                continue
+            input_number = int(raw_input)
+
+            if input_number < 1 or input_number > answers_num:
+                print(f"Bad input. Please provide numbers in range [1, {answers_num}]")
+                continue
+
+            return input_number - 1
